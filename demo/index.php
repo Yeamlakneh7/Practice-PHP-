2a0f715f40a361bab390fc8cff1e8ac1758a5bc8
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-04 22:42:50 +0300

    conditionals and boolean

--- demo/index.php
+++ demo/index.php
@@ -6,7 +6,7 @@
     <title>Demo</title>
 </head>
 <body>
-    <h1>
+    <!-- <h1>
         <?php 
             $greetings = "Hello";
             $person = "Everyone";
@@ -16,11 +16,32 @@
             echo $greetings . " " . $person;
             
         ?>
-    </h1>
+    </h1> -->
     <!-- <h2>
         <?php 
             echo "with Laracast";
         ?>
     </h2> -->
+
+    <?php 
+        $name = "The Holiness of God";
+        $read = true;
+
+        if ($read) {
+            $message = "You have read $name.";
+        }else {
+            $message = "You have NOT read $name.";
+        }
+    ?>
+
+    
+    <h1 style="display: grid; place-items: center; height: 100vh; font-family: serif;">
+    <?php 
+        echo $message . "<br>" . "<br>";
+    ?>
+    <?=$message?>
+
+    </h1>
+
 </body>
 </html>
