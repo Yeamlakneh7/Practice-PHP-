ffdaaf5c6c2140c2e22e867acb69f7d91068f388
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-14 21:21:23 +0300

    putting some changes in naming and line structure

--- demo/views/about.view.php
+++ demo/views/about.view.php
@@ -1,12 +1,11 @@
-<?php require "Partials/head.php"; ?>
+<?php require("Partials/head.php"); ?>
+<?php require("Partials/nav.php"); ?>
+<?php require("Partials/banner.php"); ?>
 
-  <?php require "Partials/nav.php"; ?>
+<main>
+<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+  <h3>This is About Us page.</h3>
+</div>
+</main>
 
-  <?php require "Partials/header.php"; ?>
-  <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h3>Let's Build A Website</h3>
-    </div>
-  </main>
-
-  <?php require "Partials/footer.php"; ?>
+<?php require("Partials/footer.php"); ?>
