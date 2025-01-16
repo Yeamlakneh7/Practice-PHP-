35f8734567f0b3da42801008b9c5ac128db5ce32
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-16 23:19:53 +0300

    This code should be fixed though

--- demo/index.php
+++ demo/index.php
@@ -1,2 +1,13 @@
 <?php
 
+require("functions.php");
+
+$uri = dd($_SERVER['REQUEST_URI']);
+
+if ($uri === '/phpProject/demo/controllers/') {
+    require("controllers/index.php");
+} else if ($uri === '/phpProject/demo/controllers/about') {
+    require("controllers/about.php");
+} else if ($uri === '/phpProject/demo/controllers/contact') {
+    require("controllers/contact.php");
+}
