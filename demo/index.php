a4e5bbb8d74dd2dcbae16ddf1d09dd58d28c958c
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-19 23:02:21 +0300

    Everything works, and well structured.

--- demo/index.php
+++ demo/index.php
@@ -1,15 +1,4 @@
 <?php
 
 require("functions.php");
-
-$url = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/demo/' => 'controllers/index.php',
-    '/demo/about' => 'controllers/about.php',
-    '/demo/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($url, $routes)) {
-    require $routes[$url];
-}
+require("routes.php");
---
+++ demo/routes.php
@@ -0,0 +1,33 @@
+<?php 
+
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/demo/' => 'controllers/index.php',
+    '/demo/about' => 'controllers/about.php',
+    '/demo/contact' => 'controllers/contact.php',
+];
+
+function routeToController($url, $routes) {
+
+    if (array_key_exists($url, $routes)) {
+    
+        require $routes[$url];
+    
+    } else{
+      
+        abort();
+
+    }
+}
+
+function abort($code = 404) {
+    
+    http_response_code($code);
+    
+    require 'views/404.php';
+
+    die();
+}
+
+routeToController($url, $routes);
