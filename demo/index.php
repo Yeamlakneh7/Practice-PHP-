3215a5e61d279bbbe51fb92d1de842cb2bc7d859
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-19 20:44:25 +0300

    Problem finally solved!  After adding .htaccess code

--- demo/index.php
+++ demo/index.php
@@ -1,12 +1,12 @@
 <?php
 
-require("functions.php"); 
+require("functions.php");
 
 if ($_SERVER['REQUEST_URI'] === "/demo/") {
     require "controllers/index.php";
-} elseif ($_SERVER['REQUEST_URI'] === "/about/") {
+} elseif ($_SERVER['REQUEST_URI'] === "/demo/about") {
     require "controllers/about.php";
-} elseif ($_SERVER['REQUEST_URI'] === "/contact/") {
+} elseif ($_SERVER['REQUEST_URI'] === "/demo/contact") {
     require "controllers/contact.php";
 } else {
     echo "404 Not Found";
--- demo/views/Partials/nav.php
+++ demo/views/Partials/nav.php
@@ -9,8 +9,8 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/demo/" class="<?= urlIs('/demo/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="/demo/about/" class="<?= urlIs('/demo/about/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700' ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>              
-              <a href="/demo/contact/" class="<?= urlIs('/demo/contact/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700' ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+              <a href="/demo/about" class="<?= urlIs('/demo/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700' ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>              
+              <a href="/demo/contact" class="<?= urlIs('/demo/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700' ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
             </div>
           </div>
         </div>
