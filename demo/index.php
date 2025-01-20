be77e06e5c48c44101149bc8ceaff77bb06acacf
Yeam <yeamlakneheshetu9@gmail.com>
2025-01-20 14:44:43 +0300

    working on  mysql connection with php

--- demo/index.php
+++ demo/index.php
@@ -1,4 +1,18 @@
 <?php
 
 require("functions.php");
-require("routes.php");
+// require("routes.php");
+
+$dsn = "mysql:host=127.0.0.1;dbname=myapp;user=root;password=MySQL@1552;";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('select * from posts');
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['Title'] . "</li>";
+}
